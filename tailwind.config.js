4fef81d823dc874f1099632dd3a66b846b018101
tobikiller <edwardnguimeya@yahoo.com>
2022-03-01 22:02:40 +0100

    Start

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        "lightDark":"#161617",
+        "ShadeWhite":"#F8F8FF",
+        "lowDark":"#282939",
+      },
+      fontFamily:{
+        "Tangerine": "Tangerine"
+      }
+    },
+  },
+  plugins: [],
+}
